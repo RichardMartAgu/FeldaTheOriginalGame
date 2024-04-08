0cc6c0d121ababb6197d6b4deea6fe79656ef424
Richard <125735052+RichardMartAgu@users.noreply.github.com>
2024-04-08 18:54:06 +0200

    Initial commit

---
+++ desktop/src/com/svalero/game/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.svalero.game;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.svalero.game.Felda;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("FeldaTheOriginalGame");
+		new Lwjgl3Application(new Felda(), config);
+	}
+}
